f889003a4011e75da4c73e64cb0f8e1974e8f0bc
scm197 <scm197@rutgers.edu>
2017-05-12 21:16:09 -0400

    1) add : train the single layer nn 2) update : variable names

--- dmlpack.hpp
+++ dmlpack.hpp
@@ -28,9 +28,9 @@ namespace dmlpack
 			matrix_op::matrix<T> _valid_x;
 			matrix_op::matrix<T> _valid_y;	
 	
-			std::size_t num_classes;
-			std::size_t num_features;
-			std::size_t num_samples;
+			std::size_t _num_classes;
+			std::size_t _num_features;
+			std::size_t _num_train_samples;
 
 		public:
 			ml()
@@ -44,9 +44,9 @@ namespace dmlpack
 				_train_y = train_y;
 				
 				// set these values to be used in the training algorithms
-				num_classes = _train_y.numCols();	
-				num_features = _train_x.numCols();
-				num_samples = _train_x.numRows();
+				_num_classes = _train_y.numCols();	
+				_num_features = _train_x.numCols();
+				_num_train_samples = _train_x.numRows();
 			}
 
 			void set_testing_data( matrix_op::matrix<T> test_x , matrix_op::matrix<T> test_y)
@@ -63,14 +63,48 @@ namespace dmlpack
 	class single_layer_nn : public ml<T>
 	{
 		public:
+			// TODO : create a better way to pass in parameters
 			single_layer_nn(std::size_t iterations , double learning_rate) 
 			{
 				_iterations = iterations;
 				_learning_rate = learning_rate;	
+
 			};
 
+			/*
+			 * precondition :
+			 * 	Only call after testing the training data
+			 *
+			 */
 			void train()
 			{
+
+				_layer.resize(ml<T>::_num_classes , ml<T>::_num_features + 1 , 0);
+
+				//matrix<T> delta_weight(num_classes , num_features + 1 , 0);		
+				for(size_t iter = 0 ; iter < _iterations ; ++iter)
+				{
+					for(size_t train_sample = 1; train_sample <= ml<T>::num_train_samples ; ++train_sample) // each row in the matrices
+					{
+						// get the feature vector 1 * n
+						matrix_op::matrix<T> feature_vec = ml<T>::_train_x.returnRow(train_sample);	
+
+						// Append the +1 towards its end. 
+						feature_vec.resize(1 , feature_vec.numCols() + 1);
+						feature_vec(1 , feature_vec.numCols()) = 1;
+					
+						// m * 1
+						matrix_op::matrix<T> actual_output_vec  = ml<T>:: _train_y.returnRow(train_sample).transpose();
+						
+						// m * 1				// m * n 		// n * 1
+						matrix_op::matrix<T> pred_output_vec = _layer * feature_vec.transpose();
+
+						//incremental change
+
+						_layer = _layer + ( ( actual_output_vec - pred_output_vec ) * feature_vec) * _learning_rate; 
+
+					}
+				}
 
 			}
 
@@ -84,6 +118,7 @@ namespace dmlpack
 		private: 
 			std::size_t _iterations;
 			double _learning_rate;				
+			matrix_op::matrix<T> _layer;
 	};
 
 }
