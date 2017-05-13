fd2b44bd3e8b74c07d9f3ce5d9d2322c304f68eb
scm197 <scm197@rutgers.edu>
2017-05-12 20:04:04 -0400

    start re arch of dmlpack class

---
+++ dmlpack.h
@@ -0,0 +1,22 @@
+/*
+ * differenct objects for handling different algorithms
+ * adding support to a single object to do everything was a bad architecture design causing a lot of pain
+ * Each algorithm will have some function redrived from a base class. 
+ *
+ */
+
+
+namespace dmlpack
+{
+	
+
+
+
+
+
+
+
+}
+
+
+
