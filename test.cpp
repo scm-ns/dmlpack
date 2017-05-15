86f507a0127a910aea6c7dac2db67ec3dc1e995e
scm197 <scm197@rutgers.edu>
2017-05-14 20:33:11 -0400

    add : simple feed forward network

--- test.cpp
+++ test.cpp
@@ -30,6 +30,32 @@ TEST_CASE("testing the matrix class")
 {
 	using namespace matrix_op;
 
+
+
+	auto feed_forward= "create a feed forward network";
+	SECTION(feed_forward)
+	{
+		auto x = matrix_op::rand_fill<float>(3, 1, 0 , 1);// 3,1
+		auto w1 = matrix_op::rand_fill<float>(4,3,0,1);   // 4 , 3
+		auto b1 = matrix_op::rand_fill<float>(4,1,0,1);   // 4 , 1
+		auto h1 = matrix_op::sigmoid(w1.mul(x) + b1); // 4,1
+		
+		auto w2 = matrix_op::rand_fill<float>(4,4,0,1); // 4 , 4
+		auto b2 = matrix_op::rand_fill<float>(4,1,0,1); // 4 , 1
+		auto h2 = matrix_op::sigmoid(w2.mul(h1) + b2);  // 4 , 1
+
+		auto w3 = matrix_op::rand_fill<float>(4,4,0,1); // 1 , 4
+		auto b3 = matrix_op::rand_fill<float>(4,1,0,1); // 1 , 1
+		auto out =  matrix_op::sigmoid(w3.mul(h2) + b3);
+		
+		std::cout << out ;
+
+	}
+
+
+
+
+
 	auto test_simple_neuron = "create a simple neuron func ";
 	SECTION(test_simple_neuron)
 	{
@@ -97,8 +123,8 @@ TEST_CASE("testing the matrix class")
 				  << weights.mul(inputs);
 */
 			auto res = (inputs.mul(weights)) + bias;
-			
 			auto firing = matrix_op::sigmoid(res);
+
 			firing.for_each([](int r, int c , float val )
 			{
 				CHECK(val >= 0); CHECK( val <= 1);
